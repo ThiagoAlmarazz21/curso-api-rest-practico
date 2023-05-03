c758553b11a052cbeafce8a288dacf6e1135d9a6
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-05-03 13:28:11 -0300

    cambio en titulos y footer

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 const API_KEY = '67149bb18cdbf8c37b40ab2088b6c8a1';
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf/8',
+  },  
+  params: {
+    'api_key': API_KEY,
+    'language': 'es-ES',
+  }
+});
 
 async function getTrendingMoviesPreview() {
-  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-  const data = await res.json();
-
+  const { data } = await api('trending/movie/day');
   const movies = data.results;
+
   movies.forEach(movie => {
     const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
@@ -24,10 +33,9 @@ async function getTrendingMoviesPreview() {
 };
 
 async function getCategoriesPreview() {
-  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY + '&language=es');
-  const data = await res.json();
-
+  const { data } = await api('genre/movie/list');
   const categories = data.genres;
+
   categories.forEach(category => {
     const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
 
